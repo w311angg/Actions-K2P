a476ab640cf84998b4e8bb15bd8a6347db137440
w311ang <30381083+w311ang@users.noreply.github.com>
2022-04-30 09:49:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,26 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default settings
 cp $GITHUB_WORKSPACE/default-settings .
 echo >> default-settings
 sed -n -i -e '/rm -rf \/tmp\/luci-modulecache\//r default-settings' -e 1x -e '2,${x;p}' -e '${x;p}' package/lean/default-settings/files/zzz-default-settings
 rm default-settings
+
+#TCP全端口代理
+sed -i "s/option tcp_redir_ports .*/option tcp_redir_ports '1:65535'/" package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/0_default_config
+
+#关闭passwall日志
+sed -i "s/option close_log_tcp '0'/option close_log_tcp '1'/" package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/0_default_config
+sed -i "s/option close_log_udp '0'/option close_log_udp '1'/" package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/0_default_config
+
+#删除ssrplus对防火墙的修改
+sed -i "/REDIRECT --to-ports 53' >>/d" package/lean/default-settings/files/zzz-default-settings
+
+#过滤代理域名IPv6
+sed -i "s/option filter_proxy_ipv6 '0'/option filter_proxy_ipv6 '1'/" package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/0_default_config
+
+#开启ChinaDNS-NG
+sed -i "/config global/a\	option chinadns_ng '1'" package/feeds/passwall/luci-app-passwall/root/usr/share/passwall/0_default_config
