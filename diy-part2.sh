a1056dceade230ed7cb461d323631af18b387ae3
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-24 22:17:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,4 +42,4 @@ sed -i "/config global/a\	option chinadns_ng '1'" package/feeds/passwallluci/luc
 sed -i "/--dport 53 -j RETURN/d" package/feeds/passwallluci/luci-app-passwall/root/usr/share/passwall/iptables.sh
 
 #chinadns-ng与mtwifi冲突解决
-patch < $GITHUB_WORKSPACE/patch/helper_dnsmasq.patch
+patch -p0 < $GITHUB_WORKSPACE/patch/helper_dnsmasq.patch
