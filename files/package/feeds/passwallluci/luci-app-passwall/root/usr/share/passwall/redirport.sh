550dc342dfa4a27d6ce1b7c7403a77d6bd62534c
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-26 09:22:15 +0800

    Delete redirport.sh

--- files/package/feeds/passwallluci/luci-app-passwall/root/usr/share/passwall/redirport.sh
+++
@@ -1,3 +0,0 @@
-#iptables -t nat -w -A PSW -m comment --comment '默认' -p tcp -m multiport --dport 85,86 -m set --match-set shuntlist dst -j REDIRECT --to-ports 1041
-#iptables -t nat -w -A PSW -m comment --comment '默认' -p tcp -m multiport --dport 85,86 -m set --match-set blacklist dst -j REDIRECT --to-ports 1041
-#iptables -t nat -w -A PSW -m comment --comment '默认' -p tcp -m multiport --dport 85,86 -m set ! --match-set chnroute dst -j REDIRECT --to-ports 1041
