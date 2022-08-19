d15d5ce4e39a46354c8e46e1e8a6e481503265c0
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-19 15:16:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,7 +54,7 @@ sed -i "s/option bind_addr.*/option bind_addr '127.0.0.1'/" package/chinadns-ng/
 sed -i "s/option trust_dns.*/option trust_dns '127.0.0.1#5335'/" package/chinadns-ng/files/chinadns-ng.config
 
 #chinadns-ng白名单改为ssrplus白名单
-sed -i "s/option chnlist_file.*/option chnlist_file '/etc/ssrplus/white.list'/" package/chinadns-ng/files/chinadns-ng.config
+sed -i "s/option chnlist_file.*/option chnlist_file '\/etc\/ssrplus\/white.list'/" package/chinadns-ng/files/chinadns-ng.config
 
 #chinadns-ng黑名单改为自定义黑名单
-sed -i "s/option gfwlist_file.*/option gfwlist_file '/etc/chinadns-ng/blacklist.txt'/" package/chinadns-ng/files/chinadns-ng.config
+sed -i "s/option gfwlist_file.*/option gfwlist_file '\/etc\/chinadns-ng\/blacklist.txt'/" package/chinadns-ng/files/chinadns-ng.config
