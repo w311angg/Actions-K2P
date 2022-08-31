5e067abfe5a5dec71e752a7e3ba73f87fda44f2e
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-31 23:56:40 +0800

    Delete reserve_mtwifi.sh

--- reserve_mtwifi.sh
+++
@@ -1,6 +0,0 @@
-changed_files="$(git diff-tree --no-commit-id --name-only -r 9483e6d)$(git diff-tree --no-commit-id --name-only -r a28334d)"
-
-while IFS= read -r line; do
-    echo reserving $line
-    git checkout 9483e6d~1 -- $line
-done <<< "$changed_files"
