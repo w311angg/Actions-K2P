1e7c6b61a6a9b16d282124187fabe6fac487ba65
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-31 22:09:19 +0800

    Update reserve_mtwifi.sh

--- reserve_mtwifi.sh
+++ reserve_mtwifi.sh
@@ -1,5 +1,6 @@
 changed_files=$(git diff-tree --no-commit-id --name-only -r 9483e6d)
 
 while IFS= read -r line; do
+    echo reserving $line
     git checkout 9483e6d~1 -- $line
 done <<< "$changed_files"
