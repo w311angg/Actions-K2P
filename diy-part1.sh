3abd1eec3837e5f2e342542b513b8ad7ad20b2e1
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-31 19:31:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 git clone -b luci https://github.com/w311ang/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 git clone https://github.com/w311ang/openwrt-tcp2udp.git package/tcp2udp
+
+#还原9483e6d对mtwifi的修改
+chmod +x $GITHUB_WORKSPACE/reserve_mtwifi.sh
+$GITHUB_WORKSPACE/reserve_mtwifi.sh
