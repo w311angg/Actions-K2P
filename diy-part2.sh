5b7d653fb159c175c2d3b35c6667037c23a8b51f
w311ang <30381083+w311ang@users.noreply.github.com>
2022-10-03 20:58:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,6 +25,9 @@ for i in $GITHUB_WORKSPACE/patches/*.patch; do patch --fuzz 3 -p0 < $i; done
 # Set permissions
 chmod +x package/feeds/helloworld/luci-app-ssr-plus/root/etc/ssrplus/iptables_config.sh
 
+# Patch firewall.user
+$GITHUB_WORKSPACE/scripts/firewall_replace.py $GITHUB_WORKSPACE/firewall.user package/lean/default-settings/files/zzz-default-settings
+
 #打开bbr加速
 sed -i "s/option bbr_cca.*/option bbr_cca '1'/" feeds/luci/applications/luci-app-turboacc/root/etc/config/turboacc
 
