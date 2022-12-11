005d1d6b2059d0995167ef9c18f7bd1f2ba5d990
w311ang <30381083+w311ang@users.noreply.github.com>
2022-12-11 17:17:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
 #echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
-git clone -b luci https://github.com/w311ang/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 git clone https://github.com/w311ang/openwrt-tcp2udp.git package/tcp2udp
 git clone https://github.com/w311ang/openwrt-ipportfwd.git package/ipportfwd
