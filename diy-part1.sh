2f496196daf78959be90b05295c035a857e25ca5
w311ang <30381083+w311ang@users.noreply.github.com>
2022-12-12 07:03:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-git checkout 5ad600f88cd3b48eafdf31becf551a83bd54fffc
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
