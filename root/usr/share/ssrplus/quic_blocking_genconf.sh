8018005ba534ba6b44ac0d0aa4ba278e6d9b8a19
w311ang <30381083+w311ang@users.noreply.github.com>
2023-01-22 12:38:14 +0800

    Update quic_blocking_genconf.sh

--- root/usr/share/ssrplus/quic_blocking_genconf.sh
+++ root/usr/share/ssrplus/quic_blocking_genconf.sh
@@ -1,6 +1,8 @@
+output_path='/var/dnsmasq.d/dnsmasq-ssrplus.d/quic_blocking.conf'
+echo -n >$output_path
 for domain in $(cat "/etc/ssrplus/quic_blocking.list"); do
   if [[ "$domain" != "" ]]; then
-    cat <<EOF >>/var/dnsmasq.d/dnsmasq-ssrplus.d/quic_blocking.conf
+    cat <<EOF >>$output_path
 ipset=/$domain/quic_blocking
 EOF
   fi
