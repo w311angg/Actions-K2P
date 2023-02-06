400137a5b22a59f382b7d683c192b6a9525da614
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-06 23:42:28 +0800

    Update iptables_config.sh

--- replace_files/ssrplus/iptables_config.sh
+++ replace_files/ssrplus/iptables_config.sh
@@ -6,12 +6,15 @@ $IPT -I SS_SPEC_WAN_AC 2 -i br-lan -p tcp --dport 53 -j RETURN
 #$IPT -I SS_SPEC_WAN_AC 1 -p udp -m multiport --dport 80,443 -m set ! --match-set china dst -j RETURN
 #iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m set ! --match-set blacklist dst -j RETURN || true
 
+#specific domain block quic
+#/usr/share/ssrplus/quic_blocking_genconf.sh
+#iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j RETURN
+
+#google one vpn bypass
+#iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp --dport 2153 -m set --match-set googlevpn_lan src -j RETURN
+
 #bropc
 #iptables -t mangle -I SS_SPEC_TPROXY 1 -m set --match-set bropc src -m set ! --match-set bplanmac src -p udp -j RETURN
 #$IPT -I SS_SPEC_WAN_FW $(($(iptables -L SS_SPEC_WAN_FW -t nat | wc -l)-2)) -m set --match-set bropc src -p tcp -m multiport --dports 22,53,587,465,995,993,143,80,443,853,9418 -j REDIRECT --to-ports 1234
 #$IPT -I SS_SPEC_WAN_FW $(($(iptables -L SS_SPEC_WAN_FW -t nat | wc -l)-2)) -m set --match-set bropc src -j RETURN
 #/usr/share/dnsforwarder-bropc/genlist.sh custom
-
-#specific domain block quic
-#/usr/share/ssrplus/quic_blocking_genconf.sh
-#iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j RETURN
