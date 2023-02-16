198244015122902b56ea3850b71857d0d64c669b
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-16 22:00:00 +0800

    Update iptables_config.sh

--- replace_files/ssrplus/iptables_config.sh
+++ replace_files/ssrplus/iptables_config.sh
@@ -7,7 +7,7 @@ $IPT -I SS_SPEC_WAN_AC 2 -i br-lan -p tcp --dport 53 -j RETURN
 #iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m set ! --match-set blacklist dst -j RETURN || true
 
 #specific domain block quic
-#/usr/share/ssrplus/quic_blocking_genconf.sh
+#/usr/share/ssrplus/quic_blocking_genconf.sh >/dev/null
 #iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j RETURN
 
 #google one vpn bypass
