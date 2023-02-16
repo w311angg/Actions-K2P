7f44b07b335061dc59c55cc64be38ff76aafb068
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-16 21:25:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,7 +27,7 @@ for i in $(find $GITHUB_WORKSPACE/patches/ -type f -regex ".*\.patch" | sort); d
 
 # Patch custom firewall config
 cp $GITHUB_WORKSPACE/custom_firewall.sh .
-sed -i 's/%custom_firewall%/r custom_firewall.sh/' package/lean/default-settings/files/zzz-default-settings
+sed -i -e '/%custom_firewall%/{r custom_firewall.sh' -e 'd}' package/lean/default-settings/files/zzz-default-settings
 rm custom_firewall.sh
 
 # Patch root folder
