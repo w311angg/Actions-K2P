984486bf6a72aded631937ca1dab2f82180d7902
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-19 05:27:01 +0800

    Update custom_firewall.sh

--- custom_firewall.sh
+++ custom_firewall.sh
@@ -1,1 +1,10 @@
+#bropc
+ipset -N bropc hash:mac 2>/dev/null
+ipset flush bropc
+#ipset add bropc <弟弟电脑mac> 2>/dev/null
+
+#google one vpn bypass
+ipset -N googlevpn_lan hash:mac 2>/dev/null
+ipset flush googlevpn_lan
+#ipset add googlevpn_lan <要使用googlevpn的设备mac> 2>/dev/null
 
