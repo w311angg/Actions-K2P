7ecc34a794730c7c5bbb2492ea161aeb5ea5b174
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-19 13:42:35 +0800

    Update when_stop.sh

--- root/usr/share/shadowsocksr/when_stop.sh
+++ root/usr/share/shadowsocksr/when_stop.sh
@@ -1,1 +1,19 @@
+uci set dnsforwarder.@arguments[0].enabled='0'
+uci commit
+/etc/init.d/dnsforwarder reload
+ps -w | grep -v "grep" | grep 'tcp2udp 127.0.0.1:5333 :5333' | awk '{print $1}' | xargs kill -9
+ipset destroy china6
 
+#bropc
+rm -rf /var/etc/dnsforwarder-bropc/
+rm -rf /tmp/dnsforwarder-bropc
+ps -w | grep -v "grep" | grep '\[dnsforwarder-br\]' | awk '{print $1}' | xargs kill -9
+
+serverIP=$(uci get shadowsocksr.$(uci get shadowsocksr.@global[0].global_server).ip)
+if [[ $(lua -e "print(luci.ip.new('192.168.0.0/16'):contains('$serverIP'))") == 'false' ]]; then
+  uci set shadowsocksr.@global[0].chinadns_forward='wan_114'
+  uci set shadowsocksr.@global[0].mydnsip="127.0.0.1"
+  uci set shadowsocksr.@global[0].mydnsport='5335'
+  uci delete dhcp.@dnsmasq[0].cachesize
+  uci commit
+fi
