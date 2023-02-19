2d527bca8a25793d457bbab7b241e13c5c0eea43
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-19 14:01:42 +0800

    Update when_start.sh

--- root/usr/share/shadowsocksr/when_start.sh
+++ root/usr/share/shadowsocksr/when_start.sh
@@ -16,7 +16,7 @@ ln -s /usr/bin/dnsforwarder /tmp/dnsforwarder-bropc
 /tmp/dnsforwarder-bropc -d -f /etc/dnsforwarder-bropc/dnsforwarder.config
 
 serverIP=$(uci get shadowsocksr.$(uci get shadowsocksr.@global[0].global_server).ip)
-if [[ $(lua -e "print(luci.ip.new('192.168.0.0/16'):contains('$serverIP'))") == 'true' ]]; then
+if [[ $(lua -e "require 'luci.ip'; print(luci.ip.new('192.168.0.0/16'):contains('$serverIP'))") == 'true' ]]; then
   uci set shadowsocksr.@global[0].chinadns_forward='127.0.0.1:5336'
   uci set shadowsocksr.@global[0].mydnsip="$serverIP"
   uci set shadowsocksr.@global[0].mydnsport='533'
