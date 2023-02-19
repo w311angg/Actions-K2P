e89e75cc5333e5171df4aec9deb1f64e46e8523d
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-19 14:51:35 +0800

    Update iptables_config.sh

--- root/etc/ssrplus/iptables_config.sh
+++ root/etc/ssrplus/iptables_config.sh
@@ -9,47 +9,57 @@ ipset -N gfwlist hash:net 2>/dev/null
 ipset -N bplan_dns hash:net 2>/dev/null; ipset flush bplan_dns; ipset add bplan_dns 223.5.5.5 2>/dev/null; ipset add bplan_dns 223.6.6.6 2>/dev/null
 ipset -N bplan_dns6 hash:net family inet6 2>/dev/null; ipset flush bplan_dns6; ipset add bplan_dns6 2400:3200::1 2>/dev/null; ipset add bplan_dns6 2400:3200:baba::1 2>/dev/null
 
-iptables -t filter -A forwarding_lan_rule -m set --match-set bplanmac src -j RETURN
-
 #specific domain block quic
 ipset -N quic_blocking hash:net 2>/dev/null
-iptables -t filter -A forwarding_lan_rule -o eth1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j REJECT
 
-#iptables -t filter -A forwarding_lan_rule -o eth1 -p udp -m multiport --dport 80,443 -m set ! --match-set china dst -m set ! --match-set whitelist dst -j REJECT
-#iptables -t filter -A forwarding_lan_rule -p tcp -d 1.1.1.1 --dport 443 -j REJECT --reject-with tcp-reset
-#iptables -t filter -A forwarding_lan_rule -p tcp -d 8.8.8.8 --dport 443 -j REJECT --reject-with tcp-reset
+iptables -t filter -N SS_SPEC_CUS_LAN_FWD
+iptables -t filter -I zone_lan_forward 1 --comment _SS_SPEC_RULE_ -j SS_SPEC_CUS_LAN_FWD
+iptables -t filter -A SS_SPEC_CUS_LAN_FWD -m set --match-set bplanmac src -j RETURN
 
-#bplan
-iptables -t nat -A prerouting_rule -p udp -m set --match-set bplanmac src -m set --match-set bplan_dns dst --dport 53 -j REDIRECT --to-ports 5336
-iptables -t nat -A prerouting_rule -p tcp -m set --match-set bplanmac src -m set --match-set bplan_dns dst --dport 53 -j REDIRECT --to-ports 5336
+#specific domain block quic
+iptables -t filter -A SS_SPEC_CUS_LAN_FWD -o eth1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j REJECT
 
-iptables -t nat -A prerouting_rule -m set --match-set bplanmac src -j RETURN
-iptables -t nat -A prerouting_rule -p udp -d 192.168.0.0/16 --dport 53 -j RETURN
-iptables -t nat -A prerouting_rule -p tcp -d 192.168.0.0/16 --dport 53 -j RETURN
+#iptables -t filter -A SS_SPEC_CUS_LAN_FWD -o eth1 -p udp -m multiport --dport 80,443 -m set ! --match-set china dst -m set ! --match-set whitelist dst -j REJECT
+#iptables -t filter -A SS_SPEC_CUS_LAN_FWD -p tcp -d 1.1.1.1 --dport 443 -j REJECT --reject-with tcp-reset
+#iptables -t filter -A SS_SPEC_CUS_LAN_FWD -p tcp -d 8.8.8.8 --dport 443 -j REJECT --reject-with tcp-reset
 
-#bropc
-iptables -t nat -A prerouting_rule -p udp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
-iptables -t nat -A prerouting_rule -p tcp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
-iptables -t nat -A prerouting_rule -m set --match-set bropc src -j RETURN
-
-iptables -t nat -A prerouting_rule -p udp --dport 53 -j REDIRECT --to-ports 53
-iptables -t nat -A prerouting_rule -p tcp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -N SS_SPEC_CUS_WAN_AC
+iptables -t nat -I PREROUTING 2 --comment _SS_SPEC_RULE_ -j SS_SPEC_CUS_WAN_AC
 
 #bplan
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p udp -m set --match-set bplanmac src -m set --match-set bplan_dns6 dst --dport 53 -j REDIRECT --to-ports 5336
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p tcp -m set --match-set bplanmac src -m set --match-set bplan_dns6 dst --dport 53 -j REDIRECT --to-ports 5336
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -m set --match-set bplanmac src -m set --match-set bplan_dns dst --dport 53 -j REDIRECT --to-ports 5336
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -m set --match-set bplanmac src -m set --match-set bplan_dns dst --dport 53 -j REDIRECT --to-ports 5336
 
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -m set --match-set bplanmac src -j RETURN
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p udp -d FC00::/7 --dport 53 -j RETURN
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p tcp -d FC00::/7 --dport 53 -j RETURN
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -m set --match-set bplanmac src -j RETURN
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -d 192.168.0.0/16 --dport 53 -j RETURN
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -d 192.168.0.0/16 --dport 53 -j RETURN
 
 #bropc
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p udp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p tcp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -m set --match-set bropc src -j RETURN
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -m set --match-set bropc src -j RETURN
 
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p udp --dport 53 -j REDIRECT --to-ports 53
-[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A prerouting_rule -p tcp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp --dport 53 -j REDIRECT --to-ports 53
+
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -N SS_SPEC_CUS_WAN_AC
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -I PREROUTING 1 --comment _SS_SPEC_RULE_ -j SS_SPEC_CUS_WAN_AC
+
+#bplan
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -m set --match-set bplanmac src -m set --match-set bplan_dns6 dst --dport 53 -j REDIRECT --to-ports 5336
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -m set --match-set bplanmac src -m set --match-set bplan_dns6 dst --dport 53 -j REDIRECT --to-ports 5336
+
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -m set --match-set bplanmac src -j RETURN
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -d FC00::/7 --dport 53 -j RETURN
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -d FC00::/7 --dport 53 -j RETURN
+
+#bropc
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p udp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp -m set --match-set bropc src --dport 53 -j REDIRECT --to-ports 5337
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -m set --match-set bropc src -j RETURN
+
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p udp --dport 53 -j REDIRECT --to-ports 53
+[ -n "$(command -v ip6tables)" ] && ip6tables -t nat -A SS_SPEC_CUS_WAN_AC -p tcp --dport 53 -j REDIRECT --to-ports 53
 
 
 #------------------
