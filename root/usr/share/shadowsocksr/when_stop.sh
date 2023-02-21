a7c8c4de9948255cdd08cb91858af4c6a63db843
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-21 21:53:56 +0800

    Update when_stop.sh

--- root/usr/share/shadowsocksr/when_stop.sh
+++ root/usr/share/shadowsocksr/when_stop.sh
@@ -9,14 +9,11 @@ ps -w | grep -v "grep" | grep '\[dnsforwarder-br\]' | awk '{print $1}' | xargs k
 rm -rf /var/etc/dnsforwarder-bropc/
 rm -rf /tmp/dnsforwarder-bropc
 
-serverIP=$(uci get shadowsocksr.$(uci get shadowsocksr.@global[0].global_server).ip)
-if [[ $(lua -e "require 'luci.ip'; print(luci.ip.new('192.168.0.0/16'):contains('$serverIP'))") == 'false' ]]; then
-  uci set shadowsocksr.@global[0].chinadns_forward='wan_114'
-  uci set shadowsocksr.@global[0].mydnsip="127.0.0.1"
-  uci set shadowsocksr.@global[0].mydnsport='5335'
-  uci delete dhcp.@dnsmasq[0].cachesize
-  uci commit
-fi
+uci set shadowsocksr.@global[0].chinadns_forward='wan_114'
+uci set shadowsocksr.@global[0].mydnsip="127.0.0.1"
+uci set shadowsocksr.@global[0].mydnsport='5335'
+uci delete dhcp.@dnsmasq[0].cachesize
+uci commit
 
 #specific domain block quic
 rm -rf /var/dnsmasq.d/dnsmasq-ssrplus.d/010_quic_blocking.conf
