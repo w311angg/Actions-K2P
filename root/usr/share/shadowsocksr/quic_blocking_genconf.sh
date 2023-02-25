d73721cee6ae4612a61d6da41e49729e1778f719
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-26 05:24:54 +0800

    Update quic_blocking_genconf.sh

--- root/usr/share/shadowsocksr/quic_blocking_genconf.sh
+++ root/usr/share/shadowsocksr/quic_blocking_genconf.sh
@@ -1,9 +1,2 @@
 output_path='/var/dnsmasq.d/dnsmasq-ssrplus.d/010_quic_blocking.conf'
-echo -n >$output_path
-for domain in $(cat "/etc/ssrplus/quic_blocking.list"); do
-  if [[ "$domain" != "" ]]; then
-    cat <<EOF >>$output_path
-ipset=/$domain/quic_blocking
-EOF
-  fi
-done
+cat /etc/ssrplus/quic_blocking.list | sed '/^$/d' | sed "/.*/s/.*/ipset=\/&\/quic_blocking/" >$output_path
