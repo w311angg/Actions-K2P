c1208bf1ce065140c0869cf491445d72c5ada1a9
w311ang <30381083+w311ang@users.noreply.github.com>
2023-02-25 12:40:32 +0800

    Update when_stop.sh

--- root/usr/share/shadowsocksr/when_stop.sh
+++ root/usr/share/shadowsocksr/when_stop.sh
@@ -16,3 +16,9 @@ uci commit
 
 #specific domain block quic
 rm -rf /var/dnsmasq.d/dnsmasq-ssrplus.d/010_quic_blocking.conf
+
+chinadns="$(uci get shadowsocksr.@global[0].chinadns_forward)"
+if [ ! -n "$chinadns" ]; then
+  ipset destroy chinalist
+  rm -rf /var/dnsmasq.d/dnsmasq-ssrplus.d/chn_list.conf
+fi
