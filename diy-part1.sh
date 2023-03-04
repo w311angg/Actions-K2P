49d7c12a4722704c6cbd0b7d28fe3ea2511601f9
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-04 21:43:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,7 +27,7 @@ hash=$(git log -1 --format="%H")
 date=$(git log -1 --format="%cd" --date=short)
 version=$(grep -oP -m1 '(?<=#define VERSION__ ").*?(?=")' main.c)
 cd -
-sed "s/{%pkg_version%,%pkg_source_date%,%pkg_source_version%}/{$version,$date,$hash}/" feeds/packages/net/dnsforwarder/Makefile
+sed -i "s/{%pkg_version%,%pkg_source_date%,%pkg_source_version%}/{$version,$date,$hash}/" feeds/packages/net/dnsforwarder/Makefile
 
 #更新ssrplus chinadns-ng
 git clone https://github.com/zfl9/chinadns-ng /tmp/chinadns-ng
@@ -36,4 +36,4 @@ hash=$(git log -1 --format="%H")
 date=$(git log -1 --format="%cd" --date=short)
 version=$(grep -oP -m1 '(?<=#define CHINADNS_VERSION "ChinaDNS-NG ).*?(?= <.*?>")' opt.c)
 cd -
-sed "s/{%pkg_version%,%pkg_source_date%,%pkg_source_version%}/{$version,$date,$hash}/" feeds/helloworld/chinadns-ng/Makefile
+sed -i "s/{%pkg_version%,%pkg_source_date%,%pkg_source_version%}/{$version,$date,$hash}/" feeds/helloworld/chinadns-ng/Makefile
