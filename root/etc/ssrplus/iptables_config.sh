598a7c8db406b8bf9a6031af16fcaf89d906199b
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-06 07:05:07 +0800

    Update iptables_config.sh

--- root/etc/ssrplus/iptables_config.sh
+++ root/etc/ssrplus/iptables_config.sh
@@ -92,8 +92,8 @@ $IPT -I SS_SPEC_WAN_AC 2 -i br-lan -p tcp --dport 53 -j RETURN
 #bropc
 iptables -t mangle -I SS_SPEC_TPROXY $(iptables -t mangle -L SS_SPEC_TPROXY | get_rule_number 'match-set china dst ! match-set blacklist dst') -m set --match-set bropc src -m set ! --match-set bplanmac src -p udp -m set --match-set blacklist dst -j TPROXY --on-port 1234 --on-ip 0.0.0.0 --tproxy-mark 0x1/0x1
 iptables -t mangle -I SS_SPEC_TPROXY $(iptables -t mangle -L SS_SPEC_TPROXY | get_rule_number 'match-set china dst ! match-set blacklist dst') -m set --match-set bropc src -m set ! --match-set bplanmac src -p udp -m set --match-set gfwlist dst -j TPROXY --on-port 1234 --on-ip 0.0.0.0 --tproxy-mark 0x1/0x1
-$IPT -I SS_SPEC_WAN_AC $(($(iptables -L SS_SPEC_WAN_AC -t nat | wc -l)-2-2)) -m set --match-set bropc src -m set --match-set gfwlist dst -j SS_SPEC_WAN_FW
-$IPT -I SS_SPEC_WAN_AC $(($(iptables -L SS_SPEC_WAN_AC -t nat | wc -l)-2-2)) -m set --match-set bropc src -j RETURN
+$IPT -I SS_SPEC_WAN_AC $(iptables -t nat -L SS_SPEC_WAN_AC | get_rule_number 'match-set china dst') -m set --match-set bropc src -m set --match-set gfwlist dst -j SS_SPEC_WAN_FW
+$IPT -I SS_SPEC_WAN_AC $(iptables -t nat -L SS_SPEC_WAN_AC | get_rule_number 'match-set china dst') -m set --match-set bropc src -j RETURN
 
 #specific domain block quic
 iptables -t mangle -I SS_SPEC_TPROXY 1 -p udp -m multiport --dport 80,443 -m set --match-set quic_blocking dst -j RETURN
