cd7610b9d6f912af1ca99bf71807aa2e4d3942fa
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-05 20:45:40 +0800

    Update genlist.sh

--- root/usr/share/dnsforwarder-bropc/genlist.sh
+++ root/usr/share/dnsforwarder-bropc/genlist.sh
@@ -1,5 +1,5 @@
 #更改时也要更改diy脚本预处理部分，文件后缀为.conf的按dnsmasq配置格式处理
-gfwlist_files='gfw_list.conf gfw_base.conf china.list'
+gfwlist_files='gfw_list.conf gfw_base.conf'
 custom_files='black.list white.list chinadns_white.list chinadns_black.list quic_blocking.list'
 
 case "$1" in
