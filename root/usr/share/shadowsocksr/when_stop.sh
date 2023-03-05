8a3641766bbc466312ed17f57cb5d4881d615f33
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-05 11:40:05 +0800

    Update when_stop.sh

--- root/usr/share/shadowsocksr/when_stop.sh
+++ root/usr/share/shadowsocksr/when_stop.sh
@@ -1,12 +1,12 @@
 #会在reload,restart,stop时运行
-ps -w | grep -v "grep" | grep '\[dnsforwarder-bp\]' | awk '{print $1}' | xargs kill -9
+ps -w | grep -v "grep" | grep '\[dnsforwarder-bp\]' | awk '{print $1}' | xargs kill -9 2>/dev/null
 rm -rf /tmp/dnsforwarder-bplan
 rm -rf /var/etc/dnsforwarder-bplan/
-ps -w | grep -v "grep" | grep 'tcp2udp 127.0.0.1:5333 :5333' | awk '{print $1}' | xargs kill -9
-ipset destroy china6
+ps -w | grep -v "grep" | grep 'tcp2udp 127.0.0.1:5333 :5333' | awk '{print $1}' | xargs kill -9 2>/dev/null
+ipset destroy china6 2>/dev/null
 
 #bropc
-ps -w | grep -v "grep" | grep '\[dnsforwarder-br\]' | awk '{print $1}' | xargs kill -9
+ps -w | grep -v "grep" | grep '\[dnsforwarder-br\]' | awk '{print $1}' | xargs kill -9 2>/dev/null
 rm -rf /var/etc/dnsforwarder-bropc/
 rm -rf /tmp/dnsforwarder-bropc
 
