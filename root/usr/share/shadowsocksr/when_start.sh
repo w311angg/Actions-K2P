2b1355544e6e4c9c83dd51a50583b3044e7beb34
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-05 15:00:32 +0800

    Update when_start.sh

--- root/usr/share/shadowsocksr/when_start.sh
+++ root/usr/share/shadowsocksr/when_start.sh
@@ -28,17 +28,3 @@ fi
 
 #specific domain block quic
 /usr/share/shadowsocksr/quic_blocking_genconf.sh >/dev/null
-
-chinadns="$(uci get shadowsocksr.@global[0].chinadns_forward)"
-if [ -n "$chinadns" ]; then
-  ipset create chinalist hash:net
-  mkdir -p /var/dnsmasq.d/dnsmasq-ssrplus.d/
-  wandns="$(ifstatus wan | jsonfilter -e '@["dns-server"][0]' || echo '')"
-  chinadns_second="$(uci get shadowsocksr.@global[0].chinadns_forward_second)"
-  case "$chinadns" in
-    "wan") sed_expression="/.*/s/.*/server=\/&\/${wandns}\nipset=\/&\/chinalist/" ;;
-    "wan_114") sed_expression="/.*/s/.*/server=\/&\/${wandns}\nserver=\/&\/${chinadns_second/:/#}\nipset=\/&\/chinalist/" ;;
-    *) sed_expression="/.*/s/.*/server=\/&\/${chinadns/:/#}\nipset=\/&\/chinalist/" ;;
-  esac
-  cat /etc/ssrplus/china.list | sed '/^$/d' | sed "$sed_expression" >/var/dnsmasq.d/dnsmasq-ssrplus.d/china.conf
-fi
