d65e2eb626a654d3d1c38c4b07ebc75ff3ec78b9
w311ang <30381083+w311ang@users.noreply.github.com>
2023-03-12 13:31:57 +0800

    Update when_start.sh

--- root/usr/share/shadowsocksr/when_start.sh
+++ root/usr/share/shadowsocksr/when_start.sh
@@ -2,7 +2,7 @@
 #重启后不会消失的需要加以判断
 ln -s /usr/bin/dnsforwarder /tmp/dnsforwarder-bplan
 mkdir -p /var/etc/dnsforwarder-bplan
-wan_dns="$(ifstatus wan | jsonfilter -e '@["dns-server"][0]' || echo '')"
+local wan_dns="$(ifstatus wan | jsonfilter -e '@["dns-server"][0]' || echo '')"
 cat /etc/dnsforwarder-bplan/dnsforwarder.config | sed "s/%wan_dns%/${wan_dns}$([[ -n '$wan_dns' ]] && echo ,)/" >/var/etc/dnsforwarder-bplan/dnsforwarder.config
 /tmp/dnsforwarder-bplan -d -f /var/etc/dnsforwarder-bplan/dnsforwarder.config >/dev/null
 (tcp2udp 127.0.0.1:5333 :5333 >/dev/null 2>&1)&
@@ -17,7 +17,7 @@ EOF
 ln -s /usr/bin/dnsforwarder /tmp/dnsforwarder-bropc
 /tmp/dnsforwarder-bropc -d -f /etc/dnsforwarder-bropc/dnsforwarder.config >/dev/null
 
-serverIP=$(uci get shadowsocksr.$(uci get shadowsocksr.@global[0].global_server).server)
+local serverIP=$(uci get shadowsocksr.$(uci get shadowsocksr.@global[0].global_server).server)
 if [[ $(lua -e "require 'luci.ip'; print(luci.ip.new('192.168.0.0/16'):contains('$serverIP'))") == 'true' ]]; then
   uci set shadowsocksr.@global[0].chinadns_forward='127.0.0.1:5336'
   uci set shadowsocksr.@global[0].mydnsip="$serverIP"
@@ -25,6 +25,9 @@ if [[ $(lua -e "require 'luci.ip'; print(luci.ip.new('192.168.0.0/16'):contains(
   uci set dhcp.@dnsmasq[0].cachesize='0'
   uci commit
 fi
+#在init.d脚本开头就通过uci获取参数值的，需要在commit之后再次获取
+dns_ip=$(uci get shadowsocksr.@global[0].mydnsip)
+dns_port=$(uci get shadowsocksr.@global[0].mydnsport)
 
 #specific domain block quic
 /usr/share/shadowsocksr/quic_blocking_genconf.sh >/dev/null
